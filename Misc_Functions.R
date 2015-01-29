c515df0db2122efb17a611fad9a6de0f4c13ff96
mwesthues <mwesthues@users.noreply.github.com>
2015-01-29 13:57:40 +0100

    Create Misc_Functions.R

---
+++ Misc_Functions.R
@@ -0,0 +1,9 @@
+# Check whether a package is installed, install it otherwise, load it in any case.
+pkgTest <- function(x)
+  {
+    if (!require(x,character.only = TRUE))
+    {
+      install.packages(x,dep=TRUE)
+        if(!require(x,character.only = TRUE)) stop("Package not found")
+    }
+  }
