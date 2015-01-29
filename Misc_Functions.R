5d57dc40eff89f2cca698438a092ea41ac1849d8
mwesthues <mwesthues@users.noreply.github.com>
2015-01-29 14:10:57 +0100

    Update Misc_Functions.R

--- Misc_Functions.R
+++ Misc_Functions.R
@@ -1,9 +1,1 @@
-# Check whether a package is installed, install it otherwise, load it in any case.
-pkgTest <- function(x)
-  {
-    if (!require(x,character.only = TRUE))
-    {
-      install.packages(x,dep=TRUE)
-        if(!require(x,character.only = TRUE)) stop("Package not found")
-    }
-  }
+
